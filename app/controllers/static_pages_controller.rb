ac9167235096ee796929381ec874d6c9089548af
jrkinouani <kinouani@gmail.com>
2016-11-08 09:30:46 +0400

    model logement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    @logements = Logement.all
   end
 
   def apropos
+  end
+
+  def galerie
+      @logement = Logement.find(params[:id])
   end
 end
---
+++ db/migrate/20161108050842_create_logements.rb
@@ -0,0 +1,26 @@
+class CreateLogements < ActiveRecord::Migration[5.0]
+  def change
+    create_table :logements do |t|
+      t.text :name
+      t.integer :price
+      t.text :desc
+      t.text :desc_home
+      t.string :city
+      t.string :postal
+      t.string :country
+      t.string :street
+      t.string :street_number
+      t.integer :surface
+      t.integer :capacity
+      t.integer :bedroom
+      t.string :image1
+      t.string :image2
+      t.string :image3
+      t.string :image4
+      t.string :image5
+      t.string :image6
+
+      t.timestamps
+    end
+  end
+end
