192f477848c1b31e1ab1b5b9b2c6194686d80e34
jrkinouani <kinouani@gmail.com>
2016-11-13 20:25:38 +0400

    hotfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20161109065411) do
 
   create_table "admins", force: :cascade do |t|
