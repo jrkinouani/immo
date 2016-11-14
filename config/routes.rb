1fd593365989c113efaefa197287088c4efff33b
jrkinouani <kinouani@gmail.com>
2016-11-14 08:26:57 +0400

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
 
   root 'static_pages#home'
-   get  "apropos" => 'static_pages#apropos'
+   get  "/apropos" => 'static_pages#apropos'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
