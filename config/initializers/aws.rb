0289fc70fb64a49fdd85b41491b628b8e3becaab
jrkinouani <kinouani@gmail.com>
2016-11-15 11:29:45 +0400

    update key aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,11 @@
 if Rails.env == 'development'
 
   CarrierWave.configure do |config|
-    
+
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     "AKIAJ7CSTU26KFM7JWOQ",
-      aws_secret_access_key: '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',
+      aws_access_key_id:     "AKIAIPEAEK2A6PDRNRIQ",
+      aws_secret_access_key: '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',
       region:                'eu-west-1'
     }
 
