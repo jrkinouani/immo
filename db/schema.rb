1e0285fd2746176205eca89326474b65342e55df
jrkinouani <kinouani@gmail.com>
2020-09-28 08:52:28 +0400

    update gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -13,72 +13,72 @@
 ActiveRecord::Schema.define(version: 20180301053412) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "logements", force: :cascade do |t|
-    t.text     "name"
-    t.integer  "price"
-    t.text     "desc"
-    t.text     "desc_home"
-    t.string   "city"
-    t.string   "postal"
-    t.string   "country"
-    t.string   "street"
-    t.string   "street_number"
-    t.integer  "surface"
-    t.integer  "capacity"
-    t.integer  "bedroom"
-    t.string   "image1"
-    t.string   "image2"
-    t.string   "image3"
-    t.string   "image4"
-    t.string   "image5"
-    t.string   "image6"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.boolean  "display"
+    t.text "name"
+    t.integer "price"
+    t.text "desc"
+    t.text "desc_home"
+    t.string "city"
+    t.string "postal"
+    t.string "country"
+    t.string "street"
+    t.string "street_number"
+    t.integer "surface"
+    t.integer "capacity"
+    t.integer "bedroom"
+    t.string "image1"
+    t.string "image2"
+    t.string "image3"
+    t.string "image4"
+    t.string "image5"
+    t.string "image6"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "display"
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.float    "price"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "ventes", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "price"
-    t.string   "desc"
-    t.string   "desc_home"
-    t.string   "city"
-    t.string   "postal"
-    t.string   "country"
-    t.string   "street"
-    t.string   "street_number"
-    t.string   "surface"
-    t.string   "bedroom"
-    t.string   "image1"
-    t.string   "image2"
-    t.string   "image3"
-    t.string   "image4"
-    t.string   "image5"
-    t.string   "image6"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "name"
+    t.integer "price"
+    t.string "desc"
+    t.string "desc_home"
+    t.string "city"
+    t.string "postal"
+    t.string "country"
+    t.string "street"
+    t.string "street_number"
+    t.string "surface"
+    t.string "bedroom"
+    t.string "image1"
+    t.string "image2"
+    t.string "image3"
+    t.string "image4"
+    t.string "image5"
+    t.string "image6"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
